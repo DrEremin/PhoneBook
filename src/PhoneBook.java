3c7d4278a7caaf0c96514726c4173a9205343eff
DrEremin <ivaneremin1979@gmail.com>
2020-10-12 14:57:44 +0300

    To add methods: checkPhoneNumber(), checkMatch(), formatName(), formatFullName(), formatPhoneNumber()
    Method public static boolean checkPhoneNumber(String)
    checks the correctness of entering the phone number.
    The criterion: presence of 11 digits in the entered line.
    
    Method public static int checkMatch
    (String, String[][], int) checks if the first argument
    matches any string of the second argument in the column
    specified in the third argument? If there are no matches,
    it returns -1, otherwise it returns the row's index of the
    match.
    
    Method public static String formatName(String) formats an
    argument (word) and returns a string with the same symbols
    as in the argument, but with the first letter in upper case
    and the rest in lower case.
    
    Method public static String formatFullName(String) splits
    the argument into separate words and uses the formatName()
    method to format as described above. It also formats
    hyphenated words so that both parts of such words start
    with a capital letter.
    
    Method public static String formatPhoneNumber(String)
    formats the argument and returns a string in the format
    +7 XXX XXX XX XX.

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -15,9 +15,11 @@ public class PhoneBook {
 
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        String item, fullname, numPhone;
-        String[][] dataBase = new String[5][2];
+        String item, fullName, numPhone;
+        String[][] dataBase = {{"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}};
         boolean isExit = true;
+        int indexRow;
+
         while (isExit) {
             System.out.println("\t\t\tМеню\t\t\t");
             System.out.println("1.\tДобавить запись");
@@ -32,10 +34,26 @@ public class PhoneBook {
             }
             switch (item.charAt(0)) {
                 case'1':
-                    System.out.println("Введите ФИО">>>);
-                    fullname = stdin.nextLine();
-
-
+                    System.out.print("Введите ФИО>>>");
+                    fullName = stdin.nextLine();
+                    if (checkName(fullName)) {
+                        fullName = formatFullName(fullName);
+                        indexRow = checkMatch(fullName, dataBase, 0);
+                        if (indexRow != -1) {
+                            System.out.printf("Номер телефона по имени %s: %s",
+                                    dataBase[indexRow][0], dataBase[indexRow][1]);
+                        } else {
+                            System.out.print("Введите номер телефона>>>");
+                            numPhone = stdin.nextLine();
+                            if (checkPhoneNumber(numPhone)) {
+                                numPhone = formatPhoneNumber(numPhone);
+                            } else {
+                                System.out.println("Введен некорректный номер телефона");
+                            }
+                        }
+                    } else {
+                        System.out.println("Введено некорректное имя");
+                    }
                     System.out.print("Press enter>>>");
                     stdin.nextLine();
                     break;
@@ -70,21 +88,65 @@ public class PhoneBook {
         //Добавить считывание ввода пользователя в цикле123
     }
 
-    public static boolean checkPhoneNumber(String phoneNumber) {
-        return true;
-    }
-
-    private static boolean checkName(String name) {
+    public static boolean checkName(String name) {
         return name.matches(
                 "([A-Za-zА-Яа-я]+([-][A-Za-zА-Яа-я]+)?\\s+){2}[A-Za-zА-Яа-я]+([-][A-Za-zА-Яа-я]+)?\\s*");
     }
 
-    public static String formatName(String name) {
-        return "";
+    public static boolean checkPhoneNumber(String phoneNumber) {
+        phoneNumber = phoneNumber.replaceAll("[^0-9]*", "");
+        System.out.println(phoneNumber);
+        return phoneNumber.length() == 11;
     }
 
-    public static String formatPhoneNumber(String number) {
-        return "";
+    public static int checkMatch(String str, String[][] Array, int column) {
+        int index = -1;
+        for (int i = 0; i < Array.length; i++) {
+            if (str.equals(Array[i][column])) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static String formatName(String name) {
+        Pattern pattern = Pattern.compile("[A-ZА-Я]");
+        Matcher match = pattern.matcher(name);
+        String temp = name.substring(0, 1);
+        if (!match.matches()) {
+            temp = temp.toUpperCase();
+        }
+        name = name.toLowerCase();
+        return name.replaceFirst(".", temp);
+    }
+
+    public static String formatFullName(String name) {
+        Pattern pattern = Pattern.compile(".+[-].+");
+        String[] partsOfWord;
+        name = name.replaceAll("\\s+", " ");
+        String[] words = name.split("\\s");
+        Matcher match = pattern.matcher("");
+        for (int i = 0; i < words.length; i++) {
+            match.reset(words[i]);
+            if (!match.matches()) {
+                words[i] = formatName(words[i]);
+            } else {
+                partsOfWord = words[i].split("-");
+                for (int j = 0; j < partsOfWord.length; j++) {
+                    partsOfWord[j] = formatName(partsOfWord[j]);
+                }
+                words[i] = String.join("-", partsOfWord);
+            }
+        }
+        return String.join(" ", words);
+    }
+
+    public static String formatPhoneNumber(String str) {
+        String number = "+7 ";
+        str = str.replaceAll("[^0-9]*", "");
+        number += str.substring(1, 4) + " " + str.substring(4, 7) +
+                " " + str.substring(7, 9) + " " + str.substring(9);
+        return number;
     }
 
     public static void add(String[][] book, String name, String number) {
